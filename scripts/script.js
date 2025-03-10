b4205902e05bb22b106180ac6a24259604fb2720
dandrichest <dandrichest@gmail.com>
2025-03-10 15:21:48 +0100

    update

--- scripts/script.js
+++ scripts/script.js
@@ -8,14 +8,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   updateFooter();
 
-  const mainnav = document.querySelector('.nav-menu');
-  const hambutton = document.querySelector('#hamburger');
+// Store the selected elements that we are going to use. This is not required but a good practice with larger programs where the variable will be referenced more than once.
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
 
-  // Add a click event listener to the hamburger button
-  hambutton.addEventListener('click', () => {
-    mainnav.classList.toggle('show');
-    hambutton.classList.toggle('show');
-  });
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+
+});
 
   // Courses Array
   const courses = [
